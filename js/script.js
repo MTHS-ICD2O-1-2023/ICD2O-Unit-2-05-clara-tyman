33fcdcd97b7734290b6fad228a81beb5eb70cb73
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-03-26 04:24:50 +0000

    html css js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Clara All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+
+function myButtonClicked () {
+  const TAX_RATE = 0.18
+
+  // input
+  const hours = parseInt(document.getElementById("hours-of-work").value)
+  const rate = parseInt(document.getElementById("rate-of-pay").value)
+
+
+  // process
+  const pay = (hours * rate) * (1.00 - TAX_RATE)
+  const taxes = (hours * rate) * TAX_RATE
+
+  // output
+  document.getElementById("pay-from-job").innerHTML = "$" + pay.toFixed(2)
+  document.getElementById("taxes-from-government").innerHTML = "$" + taxes.toFixed(2)
+}
